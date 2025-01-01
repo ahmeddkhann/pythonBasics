a2727fc8ccbeec3257bc2d4d8035730d1e10bbc1
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-01 19:22:41 +0500

    learned more things

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-print("I like Pizza!")
-print("my name is Ahmed Khan")
+#print("I like Pizza!")
+#print("my name is Ahmed Khan")
 
-firstName = "Ahmed"
-lastName = "khan" 
-print (firstName, lastName)
+#firstName = "Ahmed"
+#lastName = "khan" 
+#print (firstName + lastName)
+
+# print(f"{firstName}{lastName} likes pizza!")
+
+
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+age = int(input("How old are you? "))
+print(f"Next year, you will be {age + 1} years old!")
