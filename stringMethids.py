7fe8ae1c95eb72cd3812c1fc42c679a3c563dc48
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-02 13:04:18 +0500

    all of the string methods

---
+++ stringMethids.py
@@ -0,0 +1,39 @@
+# following are the methods that can be assigned to a string 
+
+#name = "Ahmed Khan"
+
+#result = name.len(name), the result is 10 becasue it also counts space
+#result = name.upper() # converts the string to upper case
+#result = name.lower() # converts the string to lower case
+#result = name.capitalize() # converts the first character to upper case and rest to lower case
+#result = name.title() # converts the first character of each word to upper case and rest to lower
+#result = name.count("a") # counts the number of "a" in the string
+#result = name.find("a") # returns the index of the first occurrence of "a"
+#result = name.rfind("a") # returns the index of the last occurrence of "a"
+#result = name.replace("a", "b") # replaces all "a" with "b
+#result = name.split() # splits the string into a list of words
+#result = name.strip() # removes the leading and trailing spaces from the string
+#result = name.center(20) # centers the string in a string of length 20
+#result = name.ljust(20) # left justifies the string in a string of length
+#result = name.rjust(20) # right justifies the string in a string of length
+#result = name.zfill(20) # fills the string with zeros on the left if it
+#result = name.startswith("Ahmed") # checks if the string starts with "Ahmed"
+#result = name.endswith("Khan") # checks if the string ends with "Khan"
+#result = name.isalpha() # checks if the string contains only alphabets
+#result = name.isalnum() # checks if the string contains only alphabets and numbers
+#result = name.islower() # checks if the string contains only lower case alphabets
+#result = name.isupper() # checks if the string contains only upper case alphabets
+#result = name.isdigit() # checks if the string contains only digits
+#result = name.isdecimal() # checks if the string contains only decimal digits
+#result = name.isspace() # checks if the string contains only spaces
+#result = name.isidentifier() # checks if the string is a valid identifier
+#result = name.isprintable() # checks if the string contains only printable characters
+#result = name.isnumeric() # checks if the string contains only numeric characters
+#result = name.istitle() # checks if the string contains only title case characters
+#result = name.join() # joins the string with the specified separator
+#result = name.lstrip() # removes the leading spaces from the string
+#result = name.rstrip() # removes the trailing spaces from the string
+#result = name.partition() # partitions the string into three parts
+#result = name.rpartition() # rpartitions the string into three parts
+#result = name.splitlines() # splits the string into a list of lines
+#result = name.swapcase() # swaps the case of the string
