4a4624f2292887c1eb2343dad708dcacaaedfd6a
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-02 11:03:03 +0500

    market store program

--- excercise1.py
+++ excercise1.py
@@ -1,6 +1,13 @@
 # calculate area 
 
-length = float(input(f"enter the length: "))
-width = float (input(f"enter the width: "))
-area = length * width 
-print (f"the total area is {area}")
+#length = float(input(f"enter the length: "))
+#width = float (input(f"enter the width: "))
+#area = length * width 
+#print (f"the total area is {area}")
+
+# market store
+item = input("What item would you like to have? ")
+price = float(input(f"what is the price of {item}? "))
+quantity = int(input("how much quantity do you want? "))
+total = price * quantity
+print(f"your total bill for {quantity} {item} is {total}")
