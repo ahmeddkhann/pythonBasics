7ba134d3d743eb10265766d175d0c69537bf0454
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-06 16:06:44 +0500

    while not loop

--- loops.py
+++ loops.py
@@ -1,6 +1,6 @@
 #while_loop
 
-name = input ("Enter your name ")
+name = input ("Enter your name (enter ) ")
 
 while name == "":
     print("you didn't enter your name ")
@@ -8,3 +8,9 @@ while name == "":
 else: 
     print(f"hello {name}")
 
+food = input ("enter your fav food!! (enter quit to quit) ")
+while not food == "quit":
+    print(f"your fav food is {food}")
+    food = input ("enter your fav food!! (enter quit to quit)")
+else: 
+    print(f"bye {name}")
