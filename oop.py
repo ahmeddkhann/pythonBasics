22511c0f6efa4b136f76131ec71351a6c3454a61
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-08 19:47:25 +0500

    more code done

--- oop.py
+++ oop.py
@@ -13,3 +13,25 @@ class Car:
 car1 = Car("Corolla", 2018, "white", True)
 car1.drive()
 
+class Student:
+
+    graduating_year = 2024
+    students_count = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.students_count += 1
+
+student1 = Student("Ahmed", 20)
+student2 = Student("James", 22)
+student3 = Student("Patrick", 25)
+student4 = Student("Sponegbob", 21)
+student5 = Student("William", 24)
+
+print(f"I gratuded in {Student.graduating_year} with {Student.students_count} students and they were")
+print(student1.name)
+print(student2.name)
+print(student3.name)
+print(student4.name)
+print(student5.name)
