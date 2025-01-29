cf29f9c43ddc50c42041365ab9229aaf18975958
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-29 21:04:52 +0500

    decorators in python

---
+++ oops/decorators/basic.py
@@ -0,0 +1,34 @@
+def checkCredentials(function):
+    def wrapper(*args, **kwargs):
+       checker = input("do you have CNIC? if yes enetr y if no enter n ")
+       if checker == "y" or checker == "Y":
+           print("ok you have the cnic")
+           print("let us verify the CNIC")
+           return function(*args, **kwargs)
+       elif checker == "n" or checker == "N":
+           print("ok you dont have the cnic")
+           print("we can not let you in")
+       else:
+           print("invalid input")
+    return wrapper
+
+def verifyCredentials(function):
+    def wrapper(*args, **kwargs):
+       cnic = input("enter the cnic ")
+       if cnic == "1234567890":
+           print("your cnic is verifies")
+           print("you can enter to academy")
+           return function(*args, **kwargs)
+       else:
+           print("your cnic is not verified")
+        
+    return wrapper
+
+@checkCredentials
+@verifyCredentials
+def greetings(name, profession):
+    print("welcome to python learning academy")
+    print("here you will learn about python and more languages also.")
+    print(f"so your name is {name} and your profession is {profession}")
+
+greetings("Ahmed", "programmer")
